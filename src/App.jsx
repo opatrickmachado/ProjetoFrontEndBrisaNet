aa0816264736c2eaabb68f7d3987eb87bdbb88b4
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-13 22:54:14 -0300

    Teste do componente ComicCardMini

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useEffect } from "react"
-import MarvelApi from "./services/marvelApi"
+import { ComicCardMini } from "./components"
+import GlobalStyle from './theme/globalStyle';
 
 function App() {
 
-  useEffect(() => {
-    detailComic()
-  }, [])
-
-  const detailComic = async () => {
-    const comicDetailed = await MarvelApi.get("9408")
-    console.log(comicDetailed.data)
-  }
-
   return (
     <div className="App">
-      Main app
+      <>
+        <GlobalStyle />
+        <ComicCardMini 
+          ComicDetails={
+            {
+              title: "Thor",
+              dates: [
+                {
+                  type: "onsaleDate",
+                  date: "1983-07-01T00:00:00-0400"
+                },
+                {
+                  type: "focDate",
+                  date: "-0001-11-30T00:00:00-0500"
+                },
+                {
+                  type: "unlimitedDate",
+                  date: "2013-04-29T00:00:00-0400"
+                },
+                {
+                  type: "digitalPurchaseDate",
+                  date: "2014-08-12T00:00:00-0400"
+                }
+              ]
+            }
+          }
+          ></ComicCardMini>
+        <ComicCardMini></ComicCardMini>
+      </>
     </div>
   )
 }

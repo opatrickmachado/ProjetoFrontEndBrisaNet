ace2a1c19794358d4d06c5a47c6b85fc638685e9
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-14 01:55:28 -0300

    Mudança no endpoint para aceitar mais parâmetros

--- src/config/public-api-keys.js
+++ src/config/public-api-keys.js
@@ -2,7 +2,7 @@ const API_KEYS = {
     MarvelComics: {
         publicKey: "366dfb6023a7138673b657767a02863a",
         privateKey: "ceb4bbee09de72235f1a5f033e16d387af4472f7",
-        URI: "https://gateway.marvel.com:443/v1/public/comics"
+        URI: "https://gateway.marvel.com:443/v1/public"
     },
 }
 

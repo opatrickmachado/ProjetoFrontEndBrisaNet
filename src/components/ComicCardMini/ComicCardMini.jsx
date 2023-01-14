5397e43d7b48f6df6500cd4a595b721e53bc390a
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-13 22:53:39 -0300

    Props parar mostrar detalhes no comic card

--- src/components/ComicCardMini/ComicCardMini.jsx
+++ src/components/ComicCardMini/ComicCardMini.jsx
@@ -1,16 +1,20 @@
 import { CardContainer, ComicCard, CardComicLink, CardContentFrame, CardBody } from "./ComicCardMini.styles"
 
 function ComicCardMini({ ComicDetails }) {
+    const stringDate = ComicDetails?.dates?.filter(d=> d.type === "onsaleDate").at(0)?.date.split("T").at(0)
+    const dataConvertida = (stringDate && stringDate[0] !== "-") ? new Date(stringDate) : false
+    const dataFormatada = dataConvertida ? new Date(dataConvertida).toLocaleDateString("pt-BR", {year: 'numeric', month: 'long'}) : "Data não informada"
+
     return (
         <CardContainer>
             <ComicCard>
                 <CardComicLink href="comic-url">
                     <CardContentFrame>
-                        <img src="https://terrigen-cdn-dev.marvel.com/content/prod/1x/antmanandthewaspquantumania_lob_crd_03.jpg" className="card-thumb-frame-thumbnail" />
+                        <img src={ComicDetails?.thumbnail?.path || "/images/image-not-found.jpg" } className="card-thumb-frame-thumbnail" />
                     </CardContentFrame>
                     <CardBody>
-                        <p className="card-body-headline">Titulo Comic (#174)</p>
-                        <p className="card-footer">Data Publicação</p>
+                        <p className="card-body-headline">{ComicDetails?.title || "Titulo Comic (#000)" }</p>
+                        <p className="card-footer">{ dataFormatada }</p>
                     </CardBody>
                 </CardComicLink>
             </ComicCard>

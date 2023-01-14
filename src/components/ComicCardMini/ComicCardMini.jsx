12c1be5bd823b250ae79ef10afc4c1edb2e24aa0
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-13 23:57:55 -0300

    Usar componente Link para rota

--- src/components/ComicCardMini/ComicCardMini.jsx
+++ src/components/ComicCardMini/ComicCardMini.jsx
@@ -1,6 +1,7 @@
-import { CardContainer, ComicCard, CardComicLink, CardContentFrame, CardBody } from "./ComicCardMini.styles"
+import { Link } from "react-router-dom"
+import { CardContainer, ComicCard, CardContentFrame, CardBody } from "./ComicCardMini.styles"
 
-function ComicCardMini({ ComicDetails }) {
+function ComicCardMini({ ComicDetails, id = 1 }) {
     const stringDate = ComicDetails?.dates?.filter(d=> d.type === "onsaleDate").at(0)?.date.split("T").at(0)
     const dataConvertida = (stringDate && stringDate[0] !== "-") ? new Date(stringDate) : false
     const dataFormatada = dataConvertida ? new Date(dataConvertida).toLocaleDateString("pt-BR", {year: 'numeric', month: 'long'}) : "Data não informada"
@@ -8,7 +9,7 @@ function ComicCardMini({ ComicDetails }) {
     return (
         <CardContainer>
             <ComicCard>
-                <CardComicLink href="comic-url">
+                <Link to={`/comic/${id}`}>
                     <CardContentFrame>
                         <img src={ComicDetails?.thumbnail?.path || "/images/image-not-found.jpg" } className="card-thumb-frame-thumbnail" />
                     </CardContentFrame>
@@ -16,7 +17,7 @@ function ComicCardMini({ ComicDetails }) {
                         <p className="card-body-headline">{ComicDetails?.title || "Titulo Comic (#000)" }</p>
                         <p className="card-footer">{ dataFormatada }</p>
                     </CardBody>
-                </CardComicLink>
+                </Link>
             </ComicCard>
         </CardContainer>
     )

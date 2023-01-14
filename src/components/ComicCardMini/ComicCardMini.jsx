5d55cfb8b34d454cc86ae67239c1f449111c8994
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-14 01:55:03 -0300

    Correção para exibir corretamente o ID e thum

--- src/components/ComicCardMini/ComicCardMini.jsx
+++ src/components/ComicCardMini/ComicCardMini.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom"
 import { CardContainer, ComicCard, CardContentFrame, CardBody } from "./ComicCardMini.styles"
 
-function ComicCardMini({ ComicDetails, id = 1 }) {
+function ComicCardMini({ ComicDetails }) {
     const stringDate = ComicDetails?.dates?.filter(d=> d.type === "onsaleDate").at(0)?.date.split("T").at(0)
     const dataConvertida = (stringDate && stringDate[0] !== "-") ? new Date(stringDate) : false
     const dataFormatada = dataConvertida ? new Date(dataConvertida).toLocaleDateString("pt-BR", {year: 'numeric', month: 'long'}) : "Data não informada"
 
+    console.log(ComicDetails)
+
     return (
         <CardContainer>
             <ComicCard>
-                <Link to={`/comic/${id}`}>
+                <Link to={`/comic/${ComicDetails?.id}`}>
                     <CardContentFrame>
-                        <img src={ComicDetails?.thumbnail?.path || "/images/image-not-found.jpg" } className="card-thumb-frame-thumbnail" />
+                        <img src={`${ComicDetails?.thumbnail?.path}.${ComicDetails?.thumbnail?.extension}` || "/images/image-not-found.jpg" } className="card-thumb-frame-thumbnail" />
                     </CardContentFrame>
                     <CardBody>
                         <p className="card-body-headline">{ComicDetails?.title || "Titulo Comic (#000)" }</p>

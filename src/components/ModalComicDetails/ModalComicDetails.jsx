67fa1a8deffb5ccc87f86d8aa44ae47d67f84c1c
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-16 03:57:02 -0300

    Estrutura para exibir o endereço escolhido

--- src/components/ModalComicDetails/ModalComicDetails.jsx
+++ src/components/ModalComicDetails/ModalComicDetails.jsx
@@ -1,18 +1,59 @@
-import { CoverComic, Modal } from "./ModalComicDetails.styles"
+import { useState } from "react"
+import { AddressMap } from "../../components"
+import { CoverComic, Modal, ButtonSendComic, ShowAddress } from "./ModalComicDetails.styles"
 
 function ModalComicDetails({ comic }) {
+
+    const [deliberyAddress, setDeliberyAddress] = useState("Pesquise seu endereço no mapa")
 
     const stopPropagationClick = function (ev) {
         ev.stopPropagation()
         console.log("cliclou no card")
     }
 
-    return(
+    return (
         <Modal open onClick={stopPropagationClick} >
             <div className="center">
                 <CoverComic>
-                    <img src={`${comic?.thumbnail?.path}.${comic?.thumbnail?.extension}` || "/images/image-not-found.jpg" }/>
+                    <img src={`${comic?.thumbnail?.path}.${comic?.thumbnail?.extension}` || "/images/image-not-found.jpg"} />
                 </CoverComic>
+                <div className="details">
+                    <section>
+                        <h1>Avengers: War Across Time (2023) #1</h1>
+                    </section>
+                    <section className="half-line">
+                        <h2>Published:</h2>
+                        <p>January 11, 2023</p>
+                    </section>
+                    <section className="half-line">
+                        <h2>Writer:</h2>
+                        <p>Paul Levitz</p>
+                    </section>
+                    <section className="half-line">
+                        <h2>Penciler:</h2>
+                        <p>Alan Davis</p>
+                    </section>
+                    <section className="half-line">
+                        <h2>Cover Artist:</h2>
+                        <p>Alan Davis</p>
+                    </section>
+                    <section>
+                        <p>Thor! Iron Man! Captain America! Giant-Man & the Wasp! The classic Avengers against the Hulk on the streets of New York! It’s the beginning of a showdown with Kang the Conqueror that will span the centuries! Eisner Hall of Famer Paul Levitz makes his Marvel debut (unless you count a letter in Amazing Adventures #5) teamed with acclaimed artist Alan Davis!</p>
+                    </section>
+                </div>
+                <div className="delivery-container">
+                    <AddressMap />
+                    <div className="delivery-address">
+                        <ShowAddress>
+                            <strong>Endereço para entrega da revista</strong>
+                            <span>Rua Pedro Pereira Chaves, 48. Fomento. Iguatu - CE.</span>
+                            <small>Para mudar de endereço, pesquise no mapa.</small>
+                        </ShowAddress>
+                        <ButtonSendComic className="active">
+                            Enviar Revista
+                        </ButtonSendComic>
+                    </div>
+                </div>
             </div>
         </Modal>
     )
--- src/components/ModalComicDetails/ModalComicDetails.styles.jsx
+++ src/components/ModalComicDetails/ModalComicDetails.styles.jsx
@@ -1,20 +1,128 @@
 import styled from "styled-components"
 
 const CoverComic = styled.div`
-    box-sizing: border-box;
     margin: 8px;
     width: 250px;
-    padding: 16px;
+    max-height: 378px;
     display: flex;
     flex-direction: column;
     align-self: stretch;
     border: thin solid #333;
     background: #333;
+
+    img {
+      width: 249px;
+      aspect-ratio: auto 550 / 845;
+    }
 `
 
 const Modal = styled.dialog`
-    width: 980px;
+    margin: 16px auto;
+    width: 960px;
     max-width: 90%;
+    height: 680px;
+    max-height: 90vh;
+    background: #151515;
+    border: thin solid #333;
+    overflow: auto;
+    padding-bottom: 36px;
+
+    .center {
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+
+      .details {
+        width: calc(100% - 295px);
+        padding: 16px 24px;
+        color: #fff;
+  
+        section {
+          margin-bottom: 16px;
+        }
+  
+        .half-line {
+          width: 50%;
+          display: inline-block;
+        }
+  
+        h1 {
+          font-size: 24px;
+          margin-bottom: 8px;
+        }
+    
+        h2 {
+          font-size: 18px;
+          margin-bottom: 8px;
+        }
+    
+        p {
+          font-size: 16px;
+          font-weight: 100;
+          line-height: 1.5;
+        }
+      }
+
+      .delivery-container {
+        margin: 0;
+        padding: 16px 10px 0;
+        display: flex;
+
+        .delivery-address {
+          width: 250px;
+          padding-left: 16px;
+          color: #fff;
+        }
+      }
+    }
 `
 
-export { CoverComic, Modal }
+const ShowAddress = styled.div`
+    background: #2c2a2a;
+    border: thin solid #333;
+    height: 305px;
+    display: table-cell;
+    vertical-align: middle;
+    text-align: center;
+    line-height: 1.5;
+    font-size: 1.2rem;
+    color: #eee;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    span, small, strong {
+      display: block;
+    }
+    strong {
+      font-size: 0.8rem;
+      background: #fef200;
+      padding: 8px;
+      color: #151515;
+    }
+    small {
+      font-size: 0.6rem;
+      padding: 16px;
+      color: #fff;
+      letter-spacing: 0.3;
+    }
+`
+
+const ButtonSendComic = styled.button`
+  background: transparent;
+  font-size: 1.2rem;
+  border-radius: 1px;
+  border: 2px solid #666;
+  color: #333;
+  padding: 24px 36px;
+  width: 100%;
+
+  &.active {
+    background: #f23c27;
+    border: 2px solid #f23c27;
+    color: #fff;
+    cursor: pointer;
+  }
+`
+
+export { CoverComic, Modal, ButtonSendComic, ShowAddress }

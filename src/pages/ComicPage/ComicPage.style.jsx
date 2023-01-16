b0bd16aa9fa221660c0550427858319712db2aad
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-16 01:21:20 -0300

    Criação do elemento fullscreen para inserir modal

---
+++ src/pages/ComicPage/ComicPage.style.jsx
@@ -0,0 +1,34 @@
+import styled, { keyframes } from "styled-components"
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
+const FullScreen = styled.div`
+    align-items: center;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+    z-index: 99;
+    overflow: hidden;
+    background: rgba(51,51,51,0.6);
+    backdrop-filter: blur(6px);
+    transition: all 0.3s ease-in;
+    animation: 0.4s ${fadeIn} ease-in;
+
+    .center {
+        display: flex;
+        align-items: center;
+    }
+`
+
+export { FullScreen }

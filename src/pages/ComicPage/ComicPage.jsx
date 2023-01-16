a262ac53451eb8c7403281b8145763fada47717b
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-16 01:21:48 -0300

    Exibir a rota "comic" como modal

--- src/pages/ComicPage/ComicPage.jsx
+++ src/pages/ComicPage/ComicPage.jsx
@@ -1,20 +1,31 @@
 import { useEffect, useState } from "react";
-import { useParams, useLocation  } from "react-router-dom";
+import { useParams, useLocation, Navigate  } from "react-router-dom"
+import { ModalComicDetails } from "../../components"
+import { FullScreen } from "./ComicPage.style";
 
 function ComicPage(props) {
 
     const { id } = useParams()
     const stateFromLocation = useLocation()
     const [comicData, setComicData] = useState()
+    const [showModal, setShowModal] = useState(true)
 
     useEffect(() => {
         setComicData(stateFromLocation.state.comic)
     }, [stateFromLocation])
 
+    const handleCloseModalContainer = function(ev) {
+        setShowModal(false)
+    }
+
     return (
         <>
-            Mostrar detalhes aqui da comic #{id}
-            {JSON.stringify(comicData)}
+            {!showModal && (
+                <Navigate to="/" replace={true} />
+            )}
+            <FullScreen onClick={handleCloseModalContainer}>
+                <ModalComicDetails comic={comicData}/>
+            </FullScreen>
         </>
     )
 }

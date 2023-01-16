0453a4eee1ff6eb070120b641c8fcdf9d8cab1bc
Joeldo Olinda <joeldo.olinda@gmail.com>
2023-01-15 21:23:51 -0300

    Elemento personalizado para mensagem de erro

---
+++ src/components/AddressMap/AddressMap.style.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const MapNotLoad = styled.div`
+    width: 400px;
+    height: 400px;
+    background: url("images/404.png") center center no-repeat;
+`
+
+export {MapNotLoad}
